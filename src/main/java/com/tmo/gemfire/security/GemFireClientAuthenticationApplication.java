cb36a5a7d8f5eada332b89b820ed7fe46185791d
nchandrappa <nchandrappa@pivotal.io>
2017-08-28 11:36:45 -0400

    updating CUPS

--- src/main/java/com/tmo/gemfire/security/GemFireClientAuthenticationApplication.java
+++ src/main/java/com/tmo/gemfire/security/GemFireClientAuthenticationApplication.java
@@ -2,8 +2,10 @@ package com.tmo.gemfire.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(value="tmoClientCache.xml")
 public class GemFireClientAuthenticationApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/tmo/gemfire/security/controller/HomeController.java
+++ src/main/java/com/tmo/gemfire/security/controller/HomeController.java
@@ -1,13 +1,24 @@
 package com.tmo.gemfire.security.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+	@Autowired
+	Environment env;
+
 	@RequestMapping("/")
 	public String home() {
+
+
+		System.out.println(env.getProperty("vcap.services.gemfire9.credentials.locators"));
+		System.out.println(env.getProperty("vcap.services.gemfire9.credentials.users[0].username"));
+		System.out.println(env.getProperty("vcap.services.gemfire9.credentials.users[0].password"));
+
 		return "Hello";
 	}
 
